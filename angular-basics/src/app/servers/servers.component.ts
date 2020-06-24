5b39d6c9cc6927c3dc3acc69faa9d170ccd9605d
Abhijit Paul <paulabhijit656@gmail.com>
2020-06-24 22:27:36 +0530

    NgFor Example

--- angular-basics/src/app/servers/servers.component.ts
+++ angular-basics/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'Server Was Not Created';
   serverName = 'Test Server';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName)
     this.serverCreationStatus = 'Server Was Created. Server name is : ' + this.serverName;
   }
 
